6a4ab52f7b06f92bcf1a33e6a00f4428bcc803b5
pablogre <38010408+pablogre@users.noreply.github.com>
2023-03-14 18:48:16 -0300

    url login

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	Quagga.onDetected((data) => {
 		$resultados.textContent = data.codeResult.code;
 		let cod = data.codeResult.code;
-		location.href = `/CODE_BAR?${cod}`
+		//location.href = `/CODE_BAR?${cod}`
+		location.href = `/sqldata.dyndns.org:5002/login`
 		// Imprimimos todo el data para que puedas depurar
 		console.log(data);
 	});
