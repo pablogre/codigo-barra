d98ffd6617d87f20ebc1e4733a9ebb2e4389f591
pablogre <38010408+pablogre@users.noreply.github.com>
2023-04-01 14:00:23 -0300

    MODIFICANDO ARCHIVOS

--- script.js
+++ script.js
@@ -25,7 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	Quagga.onDetected((data) => {
 		$resultados.textContent = data.codeResult.code;
 		let cod = data.codeResult.code;
-		location.href = `http://201.231.80.136:5002/CODE_BAR?${cod}`
+		let vector = []
+		vector.push(cod)
+		console.log(vector)
+		//location.href = `http://201.231.80.136:5002/CODE_BAR?${cod}`
 		
 		//location.href = `http://138.97.202.70:5002/login`
 
